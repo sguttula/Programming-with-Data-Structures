72c982f9d9a18084c0709b28708d9cf53862e8fc
Sudarshan Guttula <sidguttula@yahoo.com>
2020-02-11 18:13:01 -0800

    Distance computation

--- Labs/chapter4/OrderTwoCities.java
+++ Labs/chapter4/OrderTwoCities.java
@@ -1,12 +1,12 @@
 package chapter4;
-
+  
 import java.util.Scanner;
 
 public class OrderTwoCities {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     
-    // Prompt the user to enter two cities
+    // Prompt the user to enter two cities 
     System.out.print("Enter the first city: ");
     String city1 = input.nextLine();
     System.out.print("Enter the second city: ");
